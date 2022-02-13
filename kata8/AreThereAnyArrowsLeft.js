2792f78e6e68d24a079b580a5ff3aa63883e37eb
Iryna Vakulovska <vakulovskaya@gmail.com>
2022-02-13 09:48:02 -0800

    added task Remove first and last char

--- kata8/AreThereAnyArrowsLeft.js
+++ kata8/AreThereAnyArrowsLeft.js
@@ -1,7 +1,8 @@
 //https://www.codewars.com/kata/559f860f8c0d6c7784000119/train/javascript
-const anyArrows = arrows => {
-    for (let i = 0; i < arrows.length; i++){
-    if (!arrows[i]['damaged'])return true;
+function anyArrows(arrows) {
+  for (let i = 0; i < arrows.length; i++) {
+    if (!arrows[i]['damaged'])
+      return true;
   }
   return false;
-  }
+}
---
+++ kata8/RemoveFirstandLastCharacter.js
@@ -0,0 +1,4 @@
+//https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript 
+//Remove First and Last Character
+const removeChar = str => str.slice(1,-1);
+console.log(removeChar("string"));

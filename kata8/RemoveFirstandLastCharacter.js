b892ee7b48621e475083cabf6f5773158e301ea3
Iryna Vakulovska <vakulovskaya@gmail.com>
2022-02-14 11:44:11 -0800

    solved task sumTheString

--- kata8/RemoveFirstandLastCharacter.js
+++ kata8/RemoveFirstandLastCharacter.js
@@ -1,4 +1,10 @@
 //https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript 
 //Remove First and Last Character
-const removeChar = str => str.slice(1,-1);
-console.log(removeChar("string"));
+//const removeChar = str => str.slice(1,-1);
+//console.log(removeChar("string"));
+function removeChar(str){ 
+    let arr = str.split('');
+    arr.pop();
+    arr.shift();
+    return arr.join('')
+}
---
+++ kata8/sumTheStrings.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/5966e33c4e686b508700002d/train/javascript
+
+const sumStr = (a, b) => {
+  return (+a + +b).toString();
+};
+console.log(sumStr("5", "8"));

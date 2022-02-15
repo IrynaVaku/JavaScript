6335f6c31ce87fad4332a3d2bb7edfdb3d7497a1
Iryna Vakulovska <vakulovskaya@gmail.com>
2022-02-14 17:52:48 -0800

    number to string

---
+++ kata8/ConvertNumberToString.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+
+// Return a string of the number here!
+const numberToString = (num) => {
+  return num.toString();
+};
+console.log(numberToString(58));

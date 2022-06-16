e6dfe689fdfd988415db871eb61e95a3d556c024
irynaVaku <vakulovskaya@gmail.com>
2022-06-15 19:19:11 -0800

    solved task js

---
+++ kata8/reversedWords.js
@@ -0,0 +1,17 @@
+//https://www.codewars.com/kata/51c8991dee245d7ddf00000e/train/javascript
+//const reverseWords=(str) => str.split(' ').reverse().join(' '); this is first method
+// second method
+const reverseWords = (str) => {
+  let res = '';
+  let word = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ' ') {
+      word += str[i];
+    } else {
+      res = ' ' + word + res;
+      word = '';
+    }
+  }
+  res = word + res;
+  return res;
+};

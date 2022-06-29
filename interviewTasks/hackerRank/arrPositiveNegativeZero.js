1ab532836ab786e10350f512b814cb2d6e9b45c1
irynaVaku <vakulovskaya@gmail.com>
2022-06-28 19:52:37 -0800

    Added new task for two max value

--- interviewTasks/hackerRank/arrPositiveNegativeZero.js
+++ interviewTasks/hackerRank/arrPositiveNegativeZero.js
@@ -14,7 +14,7 @@ const plusMinus = (arr) => {
       count0++;
     }
   }
-  let ratioNeg = (countForNeg / arr.length).toFixed(6);
+  let ratioNeg = (countForNeg / arr.length).toFixed(6); // Length of the number is 6 decimal places
   let rationPos = (countForPos / arr.length).toFixed(6);
   let ration0 = (count0 / arr.length).toFixed(6);
   console.log(`${ratioNeg}\n${rationPos}\n${ration0}`);
@@ -23,32 +23,31 @@ let res = plusMinus(testArr);
 console.log(res);
 
 ///Second method
-const plusMinus1 =(arr)=>{
+const plusMinus1 = (arr) => {
   let countNeg = 0;
   let countPos = 0;
-  for(let el of arr){
-    (el < 0)? countNeg++ : countPos++
+  for (let el of arr) {
+    el < 0 ? countNeg++ : countPos++;
   }
-  let ratioNeg = (countNeg/arr.length).toFixed(6);
-  let ratioPos = (countPos/arr.length).toFixed(6);
-  let ratioZero = ((arr.length - countNeg - countPos)/arr.length).toFixed(6);
-  console.log(`${ratioNeg}\n${ratioPos}\n${ratioZero}`)
- console.log();
-  }
+  let ratioNeg = (countNeg / arr.length).toFixed(6);
+  let ratioPos = (countPos / arr.length).toFixed(6);
+  let ratioZero = ((arr.length - countNeg - countPos) / arr.length).toFixed(6);
+  console.log(`${ratioNeg}\n${ratioPos}\n${ratioZero}`);
+  console.log();
+};
 ///Third method
-  const plusMinus3 = (arr) => {
-    let countForNeg = 0;
-    let countForPos = 0;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] < 0) {
-        countForNeg++;
-      } else if (arr[i] > 0) {
-        countForPos++;
-      }
+const plusMinus3 = (arr) => {
+  let countForNeg = 0;
+  let countForPos = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      countForNeg++;
+    } else if (arr[i] > 0) {
+      countForPos++;
     }
-    let ratioNeg = (countNeg/arr.length).toFixed(6);
-  let ratioPos = (countPos/arr.length).toFixed(6);
-  let ratioZero = ((arr.length - ratioNeg - ratioPos)/arr.length).toFixed(6);
-   return (`${ratioNeg}\n${ratioPos}\n${ratioZero}`)
   }
-  
+  let ratioNeg = (countNeg / arr.length).toFixed(6);
+  let ratioPos = (countPos / arr.length).toFixed(6);
+  let ratioZero = ((arr.length - ratioNeg - ratioPos) / arr.length).toFixed(6);
+  return `${ratioNeg}\n${ratioPos}\n${ratioZero}`;
+};
---
+++ interviewTasks/hackerRank/twoMaxValue.js
@@ -0,0 +1,38 @@
+let testArray = [1, 107, 5, 1, 7, 9, 121, 4, 0, 111, 1, 5, 1020142, 2]; // answer: 1020142,121
+const twoMaxValue = (arr) => {
+  let arrSortedIncr = [...arr.sort((a, b) => a - b)];
+  let arrSortedDecr = [...arr.sort((a, b) => b - a)];
+  // return {arrSortedIncr, arrSortedDecr,arrSortedDecr}
+  return [arrSortedDecr[0], arrSortedDecr[1]];
+};
+//console.log(twoMaxValue(testArray));
+
+//Second method
+const twoMaxValue2 = (arr) => {
+  let maxMax = arr[0];
+  let maxNext = arr[0];
+  let current;
+  for (let i = 1; i < arr.length; i++) {
+    current = arr[i];
+    console.log('\nI work with current el', current);
+    console.log({ maxMax, maxNext });
+    if (current > maxMax) {
+      console.log('I see current is bigger than maxMax');
+      maxNext = maxMax;
+      console.log('New maxNext is ', maxNext);
+      maxMax = current;
+      console.log('New maxMax value is', maxMax);
+    } else {
+      console.log('I see current is less or equal maxMax');
+      console.log('I compare current with maxNext');
+      if (current > maxNext) {
+        console.log('I see current is bigger than maxNext');
+        maxNext = current;
+        console.log('New maxNext is ', maxNext);
+      } else {
+        console.log('I do not change anything');
+      }
+    }
+  }
+};
+twoMaxValue2(testArray);

b4b1e42185153d207032c832af35f1c01ce27238
irynaVaku <vakulovskaya@gmail.com>
2022-10-09 20:36:10 -0800

    solved new JS tasks with methods

---
+++ kata7/simpleEquationReversal.js
@@ -0,0 +1,24 @@
+//https://www.codewars.com/kata/5aa3af22ba1bb5209f000037/train/javascript
+function solve(eq) {
+  revEq = '';
+  currentWord = '';
+  for (let i = 0; i < eq.length; i++) {
+    if (eq[i] !== '+' && eq[i] !== '*' && eq[i] !== '/' && eq[i] !== '-') {
+      currentWord += eq[i];
+    } else {
+      revEq = eq[i] + currentWord + revEq;
+      currentWord = '';
+    }
+  }
+  revEq = currentWord + revEq;
+  return revEq;
+}
+console.log(solve('100*b/y'));
+//////////////////////////////////////////////
+function solve1(eq) {
+  return eq
+    .split(/([*\+\-\/])/)
+    .reverse()
+    .join('');
+}
+console.log(solve1('100*b/y'));
